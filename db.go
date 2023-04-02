e82d0aa715f49e9cc13e8ec910ba8a1c16e86cc9
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-04-01 21:13:33 -0500

    half-finished admin side
    i need to make sure that my last commit didn't leave dangling code in a file that isn't in master, so im pushing this commit which does contain a half-finished admin dash.

---
+++ db.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func initSqliteDB() *sqlx.DB {
+	db, err := sqlx.Connect("sqlite3", "board.db")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return db
+}
+
+func initBoardSchema(db *sqlx.DB) {
+	schema := `
+	CREATE TABLE IF NOT EXISTS discussions (
+	    id INTEGER PRIMARY KEY,
+	    author TEXT NOT NULL,
+	    message TEXT NOT NULL
+	);
+
+	CREATE TABLE IF NOT EXISTS replies (
+	    id INTEGER PRIMARY KEY,
+	    discussion_id INTEGER NOT NULL,
+	    author TEXT NOT NULL,
+	    message TEXT NOT NULL,
+	    FOREIGN KEY (discussion_id) REFERENCES discussions(id) ON DELETE CASCADE
+	);
+	`
+	_, err := db.Exec(schema)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ types.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"container/list"
+	"sync"
+
+	"golang.org/x/crypto/ssh"
+)
+
+var users = make(map[string]*user)
+var usersMutex sync.Mutex
+var messageCache *list.List
+var semverInfo = "v0.2.3"
+
+type user struct {
+	Pubkey  string          `json:"pubkey" db:"pubkey"`
+	Hash    string          `json:"hash" db:"hash"`
+	Conn    ssh.Channel     `json:"-"`
+	Ignored map[string]bool `json:"-"`
+}
+
+type discussion struct {
+	ID      int      `json:"id" db:"id"`
+	Author  string   `json:"author" db:"author"`
+	Message string   `json:"message" db:"message"`
+	Replies []*reply `json:"replies"`
+}
+
+type reply struct {
+	Author  string `json:"author" db:"author"`
+	Message string `json:"message" db:"message"`
+}
